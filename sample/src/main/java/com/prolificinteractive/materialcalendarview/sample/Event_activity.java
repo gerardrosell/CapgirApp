4fb95179138030243ae61c068d8c989940b2aad9
gerardrosell <gerard.rosell.cardus@gmail.com>
2016-12-02 15:30:34 +0100

    Calendari + EventActivity

---
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/Event_activity.java
@@ -0,0 +1,13 @@
+package com.prolificinteractive.materialcalendarview.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Event_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_activity);
+    }
+}
