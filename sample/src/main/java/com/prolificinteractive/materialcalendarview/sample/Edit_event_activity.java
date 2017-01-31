f20bf43b704b0912bd8d44cc815231ac42d7eb2b
gerardrosell <gerard.rosell.cardus@gmail.com>
2017-01-31 12:28:29 +0100

    representació data a edit_event

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/Edit_event_activity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/Edit_event_activity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,7 +21,7 @@ public class Edit_event_activity extends AppCompatActivity {
     private String key;
     private int hour;
     private String año;
-    private String mes;
+    private String mes, mes2;
     private String dia;
     private long NAct;
 
@@ -35,8 +37,11 @@ public class Edit_event_activity extends AppCompatActivity {
         key=String.format(Locale.getDefault(), "%d", (NAct+1));
         Button create_btn = (Button) findViewById(R.id.create_btn);
         final EditText nom_event = (EditText) findViewById(R.id.nom_event);
+        final TextView showDate = (TextView) findViewById(R.id.showDate);
+        mes2 = String.valueOf(Integer.parseInt(mes)+1);
+        showDate.setText(dia+"/"+mes2+"/"+año);
         //final DatePicker selectDay = (DatePicker) findViewById(R.id.selectDay);
-        final EditText selectHour = (EditText) findViewById(R.id.selectHour);
+        final TimePicker selectHour = (TimePicker) findViewById(R.id.selectHour);
         final EditText desc = (EditText) findViewById(R.id.Descripcio);
         //final int day = selectDay.getDayOfMonth();
         //final int month = selectDay.getMonth();
@@ -49,7 +54,7 @@ public class Edit_event_activity extends AppCompatActivity {
                 //final int day = selectDay.getDayOfMonth();
                 //final int month = selectDay.getMonth();
                 //final int year = selectDay.getYear();
-                final String hour = selectHour.getText().toString();
+                final String hour = "10:00";//selectHour.getText().toString();
                 final String name = nom_event.getText().toString();
                 final String descrip = desc.getText().toString();
 
