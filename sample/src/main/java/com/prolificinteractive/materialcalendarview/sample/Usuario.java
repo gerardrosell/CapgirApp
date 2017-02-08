d4fc0fdbe417134bb21ff4025986b529013387bd
gerardrosell <gerard.rosell.cardus@gmail.com>
2017-02-08 18:08:16 +0100

    friendly name listUsu

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/Usuario.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/Usuario.java
@@ -11,13 +11,13 @@ public class Usuario {
     public String nombre;
     public String email;
     public String telefono;
-    public ArrayList<String> listItems;
+    public ArrayList<String> listUsu;
 
     public Usuario(String nom, String email, String NumTel, ArrayList<String> listI){
         this.nombre = nom;
         this.email = email;
         this.telefono = NumTel;
-        this.listItems=listI;
+        this.listUsu=listI;
 
     }
 
